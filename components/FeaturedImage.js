99266847403a889f5d7850c2b6dd9dc18f49fdfd
cl-farooqaziz <dev.faz@production-dev.com>
2023-08-22 00:37:56 +0500

    blog inner page

---
+++ components/FeaturedImage.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function FeaturedImage({ post }) {
+
+    let img = '';
+
+    const defaultFeaturedImage = "https://wp23.cryscampus.com/BitsBlogs/wp-content/uploads/images/travel_icy-polar_022K.jpg";
+    const defaultWidth = "300";
+    const defaultHeight = "200";
+
+    if (post.featuredImage) {
+        let size = post.featuredImage.node.mediaDetails.sizes[0];
+        img = {
+            src: size.sourceUrl,
+            width: size.width,
+            height: size.height
+        }
+    }
+    else {
+        img = {
+            src: defaultFeaturedImage,
+            width: defaultWidth,
+            height: defaultHeight
+        }
+    }
+
+    return (
+        <div className="d-flex align-items-center justify-content-center ms-auto">
+            <Link href={`/blog/${post.slug}`} >
+                <Image src={img.src} width={img.width} height={img.height} alt={post.title} className="img-fluid" />
+            </Link>
+        </div>
+    )
+}
---
+++ pages/blog/[postSlug].js
@@ -0,0 +1,120 @@
+import Head from "next/head";
+import Link from 'next/link'
+import Image from "next/image";
+import styles from '@/styles/InnerBlog.module.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import { getPostSlugs, getSinglePost } from "@/lib/posts";
+//
+import arrow from '../../public/images/blogBanners/right-arrow.webp'
+
+
+export async function getStaticProps({ params }) {
+    const postData = await getSinglePost(params.postSlug);
+
+    let featuredImageUrl = "https://wp23.cryscampus.com/BitsBlogs/wp-content/uploads/2023/08/moz-brand-authority-768x439-1.png";
+
+
+    return {
+        props: {
+            postData,
+        }
+    };
+}
+
+export async function getStaticPaths() {
+    const postSlugs = await getPostSlugs();
+
+    return {
+        paths: postSlugs.map((s) => (
+            {
+                params: {
+                    postSlug: s.slug
+                }
+            }
+        )),
+        fallback: false
+    }
+}
+
+export default function Post({ postData }) {
+
+    return (
+        <>
+            <Head>
+                <title key="title">{postData.title}</title>
+                <meta name="description" content={postData.metaDesc} key="metadesc" />
+                <meta property="og:title" content={postData.opengraphTitle} />
+                <meta key="og-description" property="og:description" content={postData.metaDesc} />
+                <meta property="og:url" content={postData.opengraphUrl} />
+                <meta property="og:type" content={postData.opengraphType} />
+                <meta property="og:locale" content="en_IN" />
+                <meta property="og:site_name" content={postData.opengraphSiteName} />
+                <link rel="icon" href="images/icons/favicon.png" />
+            </Head>
+
+            <section className={styles.innerBlog}>
+                <Container>
+                    <Row className="justify-content-between">
+                        <Col lg={8}>
+                            <div className={styles.blogContent}>
+                                <div className={`${styles.contentHeading}`}>
+                                    <h1 className="mb-3">{postData.title} </h1>
+                                </div>
+                                <div>
+
+                                </div>
+                                <div className={`${styles.contentBody} mt-4`}
+                                    dangerouslySetInnerHTML={{ __html: postData.content }}>
+                                </div>
+                            </div>
+                        </Col>
+
+                        <Col lg={3}>
+                            <div className={`${styles.cta} mt-5 mt-lg-0`}>
+                                <div className={styles.ctaImg}>
+                                    <Link href="/blog-page" className={`${styles.ctaBtn} mt-5`}>
+                                        get custom web designs
+                                    </Link>
+                                </div>
+                            </div>
+
+                            <div className={`${styles.platForm} mt-5`}>
+                                <h5 className={styles.subTitle}>
+                                    Endorsements on different Platforms
+                                </h5>
+                                <h5 className={styles.title}>
+                                    Awards And Recognition
+                                </h5>
+                                <p className={styles.para}>
+                                    Thriving on accelerating the path to disruption and value creation in all directions has enabled
+                                    us to receive acknowledgment and recognition by leading ratings and review platforms.
+                                </p>
+                                <Link href="/blog-page" className={`${styles.arrowBtn} mt-3`}>
+                                    <span>View Reviews</span>
+                                    <Image className='img-fluid' src={arrow} alt="bitswits" />
+                                </Link>
+                            </div>
+
+                            <div className={`${styles.newsLetter} mt-5`}>
+                                <h4>Signup for</h4>
+                                <h5>Newsletter</h5>
+                                <form className="mt-4">
+                                    <div>
+                                        <input type="text" name="name" id="name" placeholder="FULL NAME" />
+                                    </div>
+                                    <div className="mt-3">
+                                        <input type="email" name="email" id="email" placeholder="EMAIL ADDRESS" />
+                                    </div>
+                                    <button type="submit" className={`${styles.arrowBtn} mt-4`}>
+                                        <span>Submit Details</span>
+                                        <Image className='img-fluid' src={arrow} alt="bitswits" />
+                                    </button>
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    );
+}
