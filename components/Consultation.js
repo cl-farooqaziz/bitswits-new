bf3659a48f58dea24d466577df0090c0772da2b4
cl-farooqaziz <dev.faz@production-dev.com>
2023-08-31 23:46:34 +0500

    consultation fixes

--- components/Consultation.js
+++ components/Consultation.js
@@ -11,9 +11,9 @@ const Consultation = () => {
                     <Row>
                         <Col lg={12}>
                             <h3 className="grdiant f-18 f-700 f-montserrat">GET A FREE CONSULTATION</h3>
-                            <h2 className="white f-50 f-700  f-playfair mt-3 mb-5">
+                            <h2 className="white f-50 f-700 f-playfair mt-3 mb-5">
                                 Looking For Expert Guidance On 
-                                Your Mobile App Development  Project?
+                                Your <span className='grdiant'>Mobile App Development</span> Project?
                             </h2>
                             <Link href='#' className={`${styles.btnConslt}`}>
                                 CLAIM YOUR COMPLIMENTARY CONSULTANCY SESSION TODAY
