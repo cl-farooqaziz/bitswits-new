457297497c47b6e4f2b6be05382d1d428fd062f8
dev-sikander <sikander@crystallitedigital.com>
2023-09-14 11:04:10 -0700

    working

--- components/metaverse/MetaverseBanner.js
+++ components/metaverse/MetaverseBanner.js
@@ -16,7 +16,7 @@ function MetaverseBanner() {
 
             <div className={`HomeBanner ${style.metaverseBanner}`}>
 
-                {/* <div className={style.overlay}></div> */}
+                <div className={style.overlay}></div>
 
                 <div className='homeVideo'>
                     <video className='' autoPlay muted loop src="../../../metaverse/images/metaBannerVideo.mov" type="video/mov"></video>
@@ -24,11 +24,11 @@ function MetaverseBanner() {
 
                 <Container>
                     <Row>
-                        <Col lg={12}>
+                        <Col lg={8} className='mx-auto'>
                             <div className={style.point}>
                                 <div className={style.Lcontent}>
                                     <h1 className='text-white mb-2 pl-4'>
-                                        <span className='fw700 font70 grdiant d-block line50'>Metaverse</span>
+                                        <span className='fw700 f-80 grdiant d-block'>Metaverse</span>
                                         <span className='fw400 font33'> Development Company </span></h1>
                                     <p className={`${style.content} text-white f15`}>Launch robust metaverse platforms with our core expertise in blockchain, AR, VR and other metaverse technologies.<br></br>
                                         Hire our top-notch metaverse experts to capture the future of digital reality. Accelerate your success in the web3 space with our metaverse development services.</p>
