d75209149d4efc85dfa998b2b210bd9483254370
dev-sikander <sikander@crystallitedigital.com>
2023-09-22 18:14:29 -0700

    banner setting

--- components/NewBanner.js
+++ components/NewBanner.js
@@ -4,48 +4,11 @@ import Image from 'next/image';
 import { Container, Row, Col } from 'react-bootstrap'
 import Link from 'next/link'
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 //
-//images
-import banImg1 from '../public/newHomePage/images/slider1.png'
-import banImg2 from '../public/newHomePage/images/slider2.png'
-import banImg3 from '../public/newHomePage/images/slider3.png'
-import banImg4 from '../public/newHomePage/images/slider4.png'
-import banImg5 from '../public/newHomePage/images/slider5.png'
-import banImg6 from '../public/newHomePage/images/slider6.png'
-import banImg7 from '../public/newHomePage/images/slider7.png'
-import banImg8 from '../public/newHomePage/images/slider8.png'
+import HomeBannerSlider from './HomeBannerSlider';
 
 const NewBanner = () => {
-
-    var bannerslider = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 3000,
-        pauseOnHover: false,
-        cssEase: 'linear',
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                }
-            }
-        ]
-    };
 
     return (
         <>
@@ -69,35 +32,9 @@ const NewBanner = () => {
                     </Row>
 
                 </Container>
-
-                <Slider {...bannerslider} className={`${styles.slider} newLogoSlide`}>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg1} className="img-fluid w-75" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg2} className="img-fluid w-75" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg3} className="img-fluid w-50" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg4} className="img-fluid w-75" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg5} className="img-fluid w-75" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg6} className="img-fluid w-50" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg7} className="img-fluid w-75" />
-                    </div>
-                    <div className={styles.strpImg}>
-                        <Image alt="bitswits" src={banImg8} className="img-fluid w-50" />
-                    </div>
-                </Slider>
             </section>
-
+            
+            <HomeBannerSlider />
 
         </>
     )
