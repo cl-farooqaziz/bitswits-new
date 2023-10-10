f6533a302b2e38cbbc0b91b770ae9c0e2ca8a7aa
alim-2023 <alim@crystallitedigital.com>
2023-10-10 16:21:49 +0500

    map tool tip

--- components/NewAddress.js
+++ components/NewAddress.js
@@ -45,10 +45,12 @@ const NewAddress = () => {
                             <h4 className='font28 grdiant2 font-bold'>Ready For Success?</h4>
                             <h5 className='font20 font-bold text-white'>Get your free 30 minutes strategy session with an experienced digital marketer valued at</h5>
                             <h6 className='font28 grdiant2 font-bold'>$300USD.</h6>
-                            <Link className={`${styles.book} ${styles.book2}`} href="#">
+                          <div className={styles.menu}>
+                          <Link className={`${styles.book} ${styles.book2}`} href="#">
                                 Get My Free 30 Min Strategy Session
                                 <Image alt="bitswits" src={arrow} className='img-fluid ms-2' />
                             </Link>
+                          </div>
                         </div>
                     </Col>
                     <Col lg={5}>
--- components/NewHomePageAbout.js
+++ components/NewHomePageAbout.js
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import { Container, Row, Col } from 'react-bootstrap'
 import Link from 'next/link'
 //
-import AboutImage from "@/public/newHomePageImages/about_image.svg"
+import AboutImage from "@/public/newHomePageImages/about_image.png"
+import comma from "@/public/newHomePageImages/comma.png"
 
 const NewHomeAbout = () => {
 
@@ -16,12 +17,20 @@ const NewHomeAbout = () => {
                 <Container>
 
                     <Row>
-                        <Col lg={12}>
+                        <Col lg={12} className={styles.hover1}>
                             <h2 className='text-white f-40 font-bold text-center'>Our Client’s Feedback Has Been</h2>
                             <h2 className='grdiant f-60 font-bold text-center'>Nothing Short Of Amazing!</h2>
-                            <div className='sec_image text-center'>
+                            <div className={` ${styles.client} sec_image text-center`}>
                                 <Image quality={75} src={AboutImage} alt='Banner' className={styles.secImage} />
+                                <div className={styles.secImage1}>
+                                    <Image src={comma} className='img-fluid' />
+                                <h4>Our iOS developers craft pixel-perfect iOS apps with <br></br> beautiful and latest designs for Apple devices. <Image src={comma} className='img-fluid' /></h4>
+
+                                <h5 className='mt-4'>James Spencer</h5>
+                                <p>CEO Jumpto1</p>
                             </div>
+                            </div >
+                           
                         </Col>
                     </Row>
 
