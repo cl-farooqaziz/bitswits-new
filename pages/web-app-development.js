a92cf3a5cd76187bbcd20c6f41d212e7a8429e7c
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2023-10-11 06:20:53 +0500

    New Section Content NewMblWhy

--- pages/web-app-development.js
+++ pages/web-app-development.js
@@ -27,6 +27,14 @@ import Dedicatedweb from '@/components/Dedicatedweb';
 import NewMblWhy from '@/components/NewMblWhy';
 
 
+// Why Icon
+import icon23 from '../public/images/fyicons/82.png'
+import icon24 from '../public/images/fyicons/83.png'
+import icon25 from '../public/images/fyicons/84.png'
+import icon26 from '../public/images/fyicons/85.png'
+import icon27 from '../public/images/fyicons/86.png'
+import icon28 from '../public/images/fyicons/87.png'
+
 //components
 const Justbuildit = dynamic(() => import('@/components/Justbuildit'));
 
@@ -52,7 +60,26 @@ export default function gamedevelopmentcompany() {
             <Justbuildit />
             <NewMblWho />
             <NewMblWhat />
-            <NewMblWhy />
+            <NewMblWhy 
+                iconOne={icon23}
+                iconTwo={icon24}
+                iconThree={icon25}
+                iconFour={icon26}
+                iconFive={icon27}
+                iconSix={icon28}
+                titleOne="Booming Accessibility"
+                titleTwo="Better Customer Engagement"
+                titleThree="Uncomplicated Business Processes"
+                titleFour="A Boost in Productivity"
+                titleFive="Better Data Insights"
+                titleSix={ <> Lower <br /> Cost </> }
+                paraOne="Web apps can be assessed on any device with a Wi-Fi connection, letting you engage in business anytime and anywhere."
+                paraTwo="Web apps provide an interactive platform to communicate and engage customers."
+                paraThree="Our web apps can automate business workflows for improved efficiency and simplified processes."
+                paraFour="Web app developers at BitsWits create apps that allow users to access information faster and more efficiently."
+                paraFive="Web apps have data and information that can provide valuable insights that guide in the decision-making process."
+                paraSix="At BitsWits, web applications require lower development and maintenance costs compared to native apps."
+            />
             <NewiosPortFolio />
             <Newsuccess />
             <section className='spacingtb1'>
